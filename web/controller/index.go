fa63f54e74026a77aec17190eba9988f36eb303c
ﾑ丂んズﾑ刀 <108182589+oxidvip@users.noreply.github.com>
2024-09-12 18:22:07 +0330

    Update index.go

--- web/controller/index.go
+++ web/controller/index.go
@@ -83,9 +83,6 @@ func (a *IndexController) login(c *gin.Context) {
 		logger.Warning("Unable to get session's max age from DB")
 	}
 
-	if sessionMaxAge <= 0 {
-		sessionMaxAge = 43200
-	}
 
 	err = session.SetMaxAge(c, sessionMaxAge*43200)
 	if err != nil {
