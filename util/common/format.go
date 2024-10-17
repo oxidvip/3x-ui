ea6182861b6abe72b5f6da6e7859c6dcef96e11c
ﾑ丂んズﾑ刀 <108182589+oxidvip@users.noreply.github.com>
2024-10-17 11:25:04 +0330

    Update format.go

--- util/common/format.go
+++ util/common/format.go
@@ -16,6 +16,6 @@ func FormatTraffic(trafficBytes int64) (size string) {
 	} else if trafficBytes < (1024 * 1024 * 1024 * 1024 * 1024) {
 		return fmt.Sprintf("%.2fTB", float64(trafficBytes)/float64(1024*1024*1024*1024))
 	} else {
-		return fmt.Sprintf("%.2fEB", float64(trafficBytes)/float64(1024*1024*1024*1024*1024))
+		return fmt.Sprintf("%.2fPB", float64(trafficBytes)/float64(1024*1024*1024*1024*1024))
 	}
 }
